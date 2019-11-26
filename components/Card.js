2803c44cdbdbfe183b1304bcbd81deefd99943f4
tjtechi <tjsulaymon@gmail.com>
2019-11-26 13:25:09 +0200

    created a crad component and used it for input container

---
+++ components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = ({ children, style }) => {
+    return <View style={{ ...styles.card, ...style }}>{children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowRadius: 6,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        elevation: 8,
+        padding: 20,
+    },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = () => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonsContainer}>
                     <Button title="Start" onPress={() => {}} />
                     <Button title="Cancel" onPress={() => {}} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
@@ -29,15 +30,8 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        shadowColor: 'black',
-        shadowRadius: 6,
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        padding: 20,
         borderRadius: 10,
-        elevation: 8,
+        alignItems: 'center',
     },
     title: {
         fontSize: 20,
