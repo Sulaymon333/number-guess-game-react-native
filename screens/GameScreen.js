382d346fa7d7d69cf57c9dca6e1ffa40fcbd3664
tjtechi <tjsulaymon@gmail.com>
2019-12-01 20:13:36 +0200

    implemented scrollView to display past guesses

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, Alert, StyleSheet } from 'react-native';
+import { View, Text, Alert, ScrollView, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 
 const generateRandom = (min, max, exclude) => {
     // exclude is the value confirmed by the user, the device is not allowed to return the user confirmed value as the random guess for the first time at least.
@@ -21,16 +22,25 @@ const generateRandom = (min, max, exclude) => {
     }
 };
 
+const renderListItem = (value, numOfRounds) => (
+    <View key={value} style={styles.listItem}>
+        <BodyText>#{numOfRounds}</BodyText>
+        <BodyText>{value}</BodyText>
+    </View>
+);
+
 const GameScreen = ({ userConfirmedValue, handleGameOver }) => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandom(1, 100, userConfirmedValue));
-    const [guessRounds, setGuessRounds] = useState(0);
+    const initialGuess = generateRandom(1, 100, userConfirmedValue);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    // const [guessRounds, setGuessRounds] = useState(0);
+    const [pastGuesses, setPastGuesses] = useState([initialGuess]);
 
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
     useEffect(() => {
         if (currentGuess === userConfirmedValue) {
-            handleGameOver(guessRounds);
+            handleGameOver(pastGuesses.length);
         }
     }, [currentGuess, userConfirmedValue, handleGameOver]);
 
@@ -45,11 +55,12 @@ const GameScreen = ({ userConfirmedValue, handleGameOver }) => {
         if (direction === 'lower') {
             currentHigh.current = currentGuess; // sets the current guessed number as the max upper boundary for the generateRandom function
         } else {
-            currentLow.current = currentGuess; // sets the current guessed number as the min lower boundary for the generateRandom function
+            currentLow.current = currentGuess + 1; // sets the current guessed number as the min lower boundary for the generateRandom function
         }
         const nextNumber = generateRandom(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
-        setGuessRounds(currentGuessRounds => currentGuessRounds + 1);
+        // setGuessRounds(currentGuessRounds => currentGuessRounds + 1);
+        setPastGuesses(currentPastGuesses => [nextNumber, ...currentPastGuesses]);
     };
 
     return (
@@ -64,6 +75,11 @@ const GameScreen = ({ userConfirmedValue, handleGameOver }) => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
+                    {pastGuesses.map((pastGuess, index) => renderListItem(pastGuess, pastGuesses.length - index))}
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -82,5 +98,26 @@ const styles = StyleSheet.create({
         width: 400,
         maxWidth: '90%',
         marginVertical: 20,
+    },
+    listContainer: {
+        flex: 1,
+        width: '80%',
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    listItem: {
+        borderColor: '#777',
+        borderWidth: 1,
+        borderRadius: 25,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
     },
 });
